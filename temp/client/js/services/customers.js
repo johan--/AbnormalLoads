7ed06fd7088e14ed7d221a8cd7b058573037d1ed
aitutaki <tomcrawley@outlook.com>
2015-02-14 10:31:30 +0000

    Fix gitignore

--- temp/client/js/services/customers.js
+++
@@ -1,6 +0,0 @@
-angular.module("abnormalloads").factory("Customers", [
-  "giCrud", function(Crud) {
-    var crud = Crud.factory("Customers", true);
-    return crud;
-  }
-]);
--- temp/client/js/services/tests.js
+++
@@ -1,6 +0,0 @@
-angular.module("abnormalloads").factory("Tests", [
-  "giCrud", function(Crud) {
-    var crud = Crud.factory("Tests", true);
-    return crud;
-  }
-]);
