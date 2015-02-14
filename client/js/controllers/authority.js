8c10ae8a6ad12b0a047cd4a4495ffbe79dd132e3
aitutaki <tomcrawley@outlook.com>
2015-02-14 16:44:14 +0000

    Corrected authority form validation (will come back to sort layout).

--- client/js/controllers/authority.js
+++ client/js/controllers/authority.js
@@ -12,15 +12,9 @@ angular.module("abnormalloads").controller("authorityController", ["$location", 
   }
 
   $scope.saveAuthority = function() {
-    if($(".ng-invalid") && $(".ng-invalid").length>0) {
-      alert('You must complete all fields before you can save.')
-    } else if($scope.data.authorityType==null) {
-      alert('You must select an authority type before you can save.')
-    } else {
-      Authorities.save($scope.data).then(function() {
-        $location.path("/list/authorities");
-      });
-    }
+    Authorities.save($scope.data).then(function() {
+      $location.path("/list/authorities");
+    });
   };
 
   $scope.deleteAuthority = function() {
